a7bb8bf156f4f7ce04283804b036fdb4953350a5
Marlon Fernandez <nico1972.mf@gmail.com>
2021-05-03 23:22:30 -0700

    chnage relationships

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
-    shows = db.relationship('Show', backref="venue", lazy=True)
+    shows = db.relationship('Show', backref=db.backref('venue'), lazy=True)
 
     def __repr__(self):
         return '<Venue {}>'.format(self.name)
@@ -66,7 +66,7 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
-    shows = db.relationship('Show', backref="artist", lazy=True)
+    shows = db.relationship('Show', backref=db.backref('artist'), lazy=True)
 
     def __repr__(self):
         return '<Artist {}>'.format(self.name)
@@ -76,10 +76,11 @@ class Show(db.Model):
     __tablename__ = 'shows'
 
     id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey(
         'artist.id'), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
+    
 
     def __repr__(self):
         return '<Show {}{}>'.format(self.artist_id, self.venue_id)
